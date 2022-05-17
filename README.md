58475464d1be11657dd32532ab628897400a06c7
Samantha Durdy <samanthadurdy@gmail.com>
2022-05-17 21:34:09 +0100

    Added worked examples of how experiments were done and utlities for making loco-cv splits, performing loco-cv and featurising data

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # KernelisedLOCO-CV
+
 A repository for code associated with the the publication "Random projections and Kernelised Leave One Cluster Out Cross-Validation: Universal baselines and evaluation tools for supervised machine learning for materials properties"
+
+** NOTE: this repository is currently missing data due to file size issues, we will shortly be uploading the data in a more compressed form along with a preprocessing script to extract and featurise it. **
+
+## Requirements
+
+This requires a working installations of:
+* python (tested using 3.7.9)
+
+* numpy (tested using version 1.21.5)
+* pandas (tested using version 1.2.3)
+* scipy (tested using verseion 1.6.1)
+* sci-kit learn (tested using version 2.4.1)
+
+* git (only to run the git clone command listed below)
+
+## Setup
+We use code from an [existing library](https://github.com/rynmurdock/domain_knowledge) to download it run
+git clone https://github.com/rynmurdock/domain_knowledge.git 
+In the top level directory of this repository.
+
+## Utilities
+in utilities.py we provide methods to do the following:
+* Featurise data (this mostly a light wrapper on ryanmurdock 's existing library)
+* Make a LOCO-CV clustering file given data
+* Perform LOCO-CV given a clustering file and some data
+
+## Notebooks
+We provide tutorial notebooks:
+* comparing_CBFV_performance.ipynb takes you through an example of comparing two different featurisation methods for one of the tasks. It uses 80/20 train/test splits, then LOCO-CV and Kernelised LOCO-CV. It then compares each featurisation method to a random projection of the same size
+* prepare_loco_cv_splits.ipynb goes through an example of preparing clusterings for use with LOCO-CV both with and without application of the radial basis function
+* create_and_apply_random_projection.ipynb takes you through the process of creating and applying a random projection (**note: random projections used for scores reported in the associated paper are found in the data/linear_seperability/random_projections folder**)
+* analyse_linear_seperability.ipynb takes you through an example of comparing the standard deviation in cluster size (cluster size uneveness) and the mean distance between a point in a cluster its centroid (spread of cluster) before and after application of radial basis function
+
+## Data 
+We are currently facing some issues with uploading large files to github, will get a workarround sorted in the next day or so that data can be included in this repository
+
+original data sources can also be found in the data availability statement, and the featurisation method used is provided in utilities.py
+
+Data for section 3.1 can be found in data/case_studies it is split using either LOCO-CV (kernelised or otherwise) or in an 80/20 train/test split
+Data for section 3.2 can be found in data/linear_seperability/datasets these datasets are not split into training/test categories (this is not applicable here), and some datasets do not have target values associated with them. The ICSD is also included in these datasets **note:** 3 of the tasks in case_studes (T_c, T_c>10K and T_c|T_c>10k) use the same base data and can be seen in the data/linear_seperability/datasets/supercon folder. 
+Data for section 3.3 can be found in data/linear_seperability/dataset/random_projectons_applied_to_datasets
---
+++ utilities.py
@@ -0,0 +1,160 @@
+"""
+utilities associated with the paper "Random projections and kernelised
+Leave One Cluster Out Cross-Validation: Universal baselines and evaluation
+tools for supervised machine learning for materials properties"
+
+"""
+import regex as re
+from sklearn.cluster import KMeans
+import pandas as pd
+try:
+    import domain_knowledge.utils.composition as dk
+    dk.path=r'domain_knowledge/data/element_properties/'
+except Exception as e:
+    raise Exception('domain_knowledge library not found. Please read and follow the instructions in the README.md to download it') from e
+with open('periodictable.csv') as f:
+    periodic_table = [x.rstrip() for x in f.readlines()]
+element_regex = re.compile(r'(([A-Z][a-z]?)\s?([0-9]*(\.[0-9]*)?)?\s?)')
+def find_clusterings(data, formulae):
+    """clusters data using kmeans clustering for values of k between 2 and 10
+
+    Parameters:
+    data (pandas Dataframe or np.ndarray): data to cluster
+    formulae (pandas Series or list of strings): formulae associated with each row of data
+    Returns:
+    list: clusters in the form [{'k':2, 'formulae':['H2O','NaCl'....],
+          'clusters':[0,1...]},{'k':2...]
+
+   """
+    clusters = []
+    for k in range(2,11):
+        km = KMeans(n_clusters=k)
+        labels = km.fit_predict(data)
+        clusters.append({'k':k, # How many clusters
+                         'labels':labels.tolist(), #Labels for each data point
+                         'formula':list(formulae), # Associated formula
+                         'centers':km.cluster_centers_.tolist() #Cluster centers
+                        })
+    return clusters
+
+def do_loco_cv(clusters, data, model, metric, return_score_breakdown=False):
+    """Performs LOCO-CV given predefined clusters
+
+    Parameters:
+    clusters (python list): Clusters with which to apply LOCO-CV
+         in the form [{'k':2, 'formulae':['H2O','NaCl'....],'clusters':[0,1...]},{'k':2...]
+    data (pandas DataFrame): data to apply LOCO-CV to
+    model (any model that uses sklearn style .fit, .predict interface): the model to evaluate
+    metric (function that takes in true and predicted values): metric to evaluate model with
+    return_score_breakdown (bool): whether to return per cluster scores as well
+         as the overall mean score of the model
+    Returns:
+    float or float, list if return_score_breakdown: the performance of the model
+           (and the associated per cluster scores if return_score_breakdown)
+
+   """
+    all_scores = []
+    #For each value of k
+    for clustering in clusters:
+
+        clustering_scores = []
+        labels = pd.Series(clustering['labels'])
+        #For each cluster
+        for i in range(clustering['k']):
+            train_df = data[labels!=i]
+            test_df = data[labels==i]
+
+            #Train the model
+            train_x = train_df.drop(['target','formula'], axis=1)
+            train_y = train_df['target']
+            model.fit(train_x, train_y)
+
+            #Make predictions on test set
+            test_x = test_df.drop(['target','formula'], axis=1)
+            test_y = test_df['target']
+            predictions = model.predict(test_x)
+
+            clustering_scores.append(metric(test_y,predictions))
+        all_scores.append(clustering_scores)
+
+    per_clustering_means = [sum(x)/len(x) for x in all_scores]
+    overall_mean = sum(per_clustering_means)/len(per_clustering_means)
+
+    if return_score_breakdown:
+        return overall_mean, per_clustering_means
+
+    return overall_mean
+
+
+
+
+def featurise_data(formulae, style='jarvis', target=None):
+    """Featurises data, with optional target to align data to
+
+    Parameters:
+    formulae (pandas DataFrame): the formulae to featurise
+    style ('jarvis', 'compVec','onehot','random_200','magpie', 'oliynyk'):
+         the featurisation method to use
+    target (pandas Series): target values to be assigned into a column
+         (called target) in the featurised data
+    Returns:
+    pandas DataFrame of featurised data
+
+   """
+    if style.lower() == 'compvec':
+        return generate_features_compVec(formulae, target=target)
+
+    if not isinstance(formulae, pd.DataFrame):
+        df = pd.DataFrame(data=formulae, columns=['formula'])
+    else:
+        df = formulae
+
+    if target is None:
+        df['target'] = 0
+    else:
+        df['target'] = target
+
+    X, Y, formulae = dk.generate_features(df, features_style=style, reset_index=True)
+    if target is not None:
+        X['target'] = target
+    X['formula'] = formulae
+    return X
+
+#This function is pretty basic but also runs pretty fast because regex is fast
+def generate_features_compVec(formulae, target = None):
+    """Creates a fractional encoding of input formulae.
+    Note: this function does not support formulae containing brackets
+
+    Parameters:
+    formulae (pandas DataFrame): the formulae to featurise
+    target (pandas Series): target values to be assigned into a column (called target) in the featurised data
+    Returns:
+    pandas DataFrame of featurised data
+
+   """
+    out = []
+    for i, formula in enumerate(formulae['formula']):
+
+        matches = element_regex.findall(formula)
+        formula_dict = {x:0.0 for x in periodic_table}
+        total=0
+        for match in matches:
+            #Deal with dutereum and tritium
+            el = 'H' if match[1] == 'D' or match[1] == 'T'  else match[1]
+            if el not in formula_dict:
+                print(f'Unkown element {el} in formula {formula}')
+                continue
+            formula_dict[el] = 1.0 if len(match[2]) == 0 else float(match[2])
+            total +=formula_dict[el]
+        try:
+            formula_dict = {x:formula_dict[x]/total for x in formula_dict}
+        except Exception as e:
+            print(formula, formula_dict)
+            break
+
+        if target is not None:
+            formula_dict['target'] = list(target)[i] #cast to a list as this could be a series with non-numeric different indices
+        formula_dict['formula'] = formula
+        out.append(formula_dict)
+
+    return pd.DataFrame.from_dict(out, orient='columns')
